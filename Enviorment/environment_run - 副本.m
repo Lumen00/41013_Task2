a26f44d06396124cb42db6caeb0df81f6d37d7b7
Zongwei Li <40359747+CK2player@users.noreply.github.com>
2022-10-07 15:33:28 +1100

    Add files via upload

---
+++ Enviorment/environment_run - 副本.m
@@ -0,0 +1,11 @@
+
+% clf;clc;clear all;close all;
+
+%requires optimization tool box add on
+
+% Turn on a light (only turn on 1, don't keep turning them on), and make axis equal
+camlight;
+view(3)
+axis equal;
+hold on;
+
