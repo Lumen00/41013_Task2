c25234ff2b8a58ca3a3dbc4b82cc6002706e4a7f
Damian Sue <113498238+Sevenstar27@users.noreply.github.com>
2022-10-20 21:39:27 +1100

    Add files via upload

--- workspace.m
+++ workspace.m
@@ -91,7 +91,7 @@ gripper = SerialLink([L1], 'name', 'master');
 
 % Set up robots.
 r = RozumPulse75();
-r.model.base = r.model.base*transl(0,-0.8,0.5+0.35);
+r.model.base = r.model.base*transl(0,-0.8,0.85);
 r.model.animate(ones(1,6));
 
 d = Dobot(false);
@@ -135,10 +135,6 @@ d.model.animate(zeros(1,6));
     set(counterCollection_h, 'Vertices', counterCollectionCoords(:,1:3));
 
     
-% Plot safety barriers on countertops.
-
-
-
 % Plot Meal Box next to Rozum bot.
 
     % Code Snippet for the closed box to be used after pushed into folding
@@ -163,13 +159,37 @@ d.model.animate(zeros(1,6));
 
 
 
+% Plot safety glass walls. Use these for link collision checks later.
+
+yConstant = r.model.base(2,4)+2.4;
+xHighBound = r.model.base(1,4)+0.4;
+xLowBound = r.model.base(1,4)-0.4;
+zHighBound = r.model.base(3,4)+1;
+zLowBound = r.model.base(3,4);
+SafetyGlass1_h = surf([xLowBound,xLowBound;xHighBound,xHighBound],[yConstant,yConstant;yConstant,yConstant],[zLowBound,zHighBound;zLowBound,zHighBound],'CData',imread('Items\SafetyGlass.png'),'FaceColor','texturemap', 'FaceAlpha', 0.2);
+
+xConstant = r.model.base(1,4)+0.4;
+yHighBound = r.model.base(2,4)+2.4;
+yLowBound = r.model.base(2,4)+0.7;
+zHighBound = r.model.base(3,4)+1;
+zLowBound = r.model.base(3,4);
+SafetyGlass2_h = surf([xConstant,xConstant;xConstant,xConstant],[yLowBound,yLowBound;yHighBound,yHighBound],[zLowBound,zHighBound;zLowBound,zHighBound],'CData',imread('Items\SafetyGlass.png'),'FaceColor','texturemap', 'FaceAlpha', 0.2);
+
+
 % Sensor in the meal box placement area to protect personnel and shut off
 % the Rozum's movement (if it's doing anything).
 
 
+% Set up hazard area for robot
+floor = r.model.base(3,4)-0.84;
+xHighBound = r.model.base(1,4)+1.5;
+xLowBound = r.model.base(1,4)-1.5;
+yHighBound = r.model.base(2,4)+2.4;
+yLowBound = r.model.base(2,4)-1;
+hazardFloor_h = surf([xLowBound,xLowBound;xHighBound,xHighBound],[yLowBound,yHighBound;yLowBound,yHighBound],[floor,floor;floor,floor],'CData',imread('Items\hazardFloor.jpg'),'FaceColor','texturemap');
 
 drawnow;
-    %% Perform Task
+%% Perform Task
 
 axis equal;
 
@@ -177,6 +197,72 @@ q1 = zeros(1,6);
 q2 = ones(1,6);
 
 view(3);
+
+%% Navigation for ROZUM picking up soft drink. (In progress!)
+
+% Decide which drink to pick up (cola, orange, lemon)
+%softDrinkColaCoords
+%softDrinkOrangeCoords
+%softDrinkLemonCoords
+
+% Extract the transform of the attach point of the soft drink.
+
+softDrinkTr = softDrinkColaCoords*troty(-pi/2);
+
+% Define the approach transform to the soft drink (away by 0.1 m in x axis)
+
+softDrinkApproachTr = softDrinkTr*transl(0,0,-0.1);
+
+% Define the initial pose to select the drink before approach.
+
+qGuess = deg2rad([0, 0, 120, 60, 90,-180]);
+
+% For the approach and pickup transforms, get their poses. Use initial
+% guesses for best results.
+
+qApproach = r.model.ikcon(softDrinkApproachTr, qGuess);
+qPickup = r.model.ikcon(softDrinkTr, qApproach);
+
+r.Travel(zeros(1,6), qApproach , 50);
+
+r.Travel(qApproach, qPickup, 30);
+
+% Take the pickup transform and lift it by 0.1 m in the local x.
+drinkLiftTr = softDrinkTr*transl(0.1,0,0);
+
+% Move to above the meal box for insertion. May require initial guess for
+% good joint lengths to avoid funky collisions.
+drinkBoxAboveTr = mealBox_OpenCoords*transl(0.07,0,0.35)*trotx(pi);
+
+qGuess = deg2rad([80, 20, 90, -20, -90, 0]);
+
+% Animate RMRC computed
+
+% Move the soft drink out of the dispenser. Use RMRC to control velocity.
+colaPickup = true;
+lastPose = RMRC(r, softDrinkTr, drinkLiftTr, qPickup, 40, 0.2);
+
+% Move the soft drink to over the meal box.
+qAbove = r.model.ikcon(drinkBoxAboveTr, qGuess);
+r.Travel(lastPose, qAbove, 100);
+
+% Move the soft drink into the meal box and deposit it. Use RMRC to control
+% velocity.
+lastPose = RMRC(r, drinkBoxAboveTr, drinkBoxAboveTr*transl(0,0,0.5), qAbove, 40, 0.2);
+colaPickup = false;
+
+% Move the robot out of the box.
+lastPose = RMRC(r, drinkBoxAboveTr*transl(0,0,0.5), drinkBoxAboveTr, lastPose, 40, 0.2);
+
+% Return to neutral pose.
+
+r.Travel(lastPose, zeros(1,6), 50);
+
+%% Navigation for ROZUM picking up cutlery (in progress)
+
+
+%% Navigation for handover of meal box.
+
 
 pickup = deg2rad([0, 90, 30, 10, -30, 85]);
 dropoff1 = [-1, 0, 0.5236, 0.1745, 0.8657, 1.4835];
@@ -191,11 +277,64 @@ DTravel(d, pickup, dropoff1, 50, 1);
 DTravel(d, dropoff1, dropoff2, 50, 1);
 DTravel(d, dropoff2, dropoff1, 50, 1);
 DTravel(d, dropoff1, pickup, 50, 1);
-d.model.teach;
 
-%RMRC(r,)
+%% VisualServoing test
+VisualServoingSafety(r, d);
 
+
+%%
 %UniversalTravel(r, q1, q2, [20, 80], d, q1, [-1, ones(1,5)], [1, 50], 1);
+
+
+%% VisualServoingSafety
+% Start the visual servoing safety demonstration.
+function VisualServoingSafety(r, d)
+    % the Rozum will hold the safety sign that the Dobot has to retreat
+    % from. The Dobot will have a camera mounted to its end effector. It is
+    % assumed it occupies the same space as the Dobot's gripper.
+
+    % Load in the safety sign and attach it to the Rozum's end effector at
+    % a pose where the dobot can see it.'
+
+    rozumTargetPose = deg2rad([104, 0, -101, 10.8, 90, 0]);
+    dobotStartPose = deg2rad([0, 90, 30, 10, -30, 85]);
+
+    r.Travel(r.model.getpos, zeros(1,6), 40); % Move Rozum to safe pose first.
+
+    UniversalTravel(r, zeros(1,6), rozumTargetPose, [1, 40], d, d.model.getpos, dobotStartPose, [1, 40], 1);
+
+    % Now that both robots are in their correct poses, get end effector
+    % position of the Rozum.
+
+    Tr = r.model.fkine(rozumTargetPose);
+    
+    yConstant = Tr(2,4);
+    xHighBound = Tr(1,4)-0.11;
+    xLowBound = Tr(1,4)+0.11;
+    zHighBound = Tr(3,4)-0.1;
+    zLowBound = Tr(3,4);
+    VSSign_h = surf([xLowBound,xLowBound;xHighBound,xHighBound],[yConstant,yConstant;yConstant,yConstant],[zLowBound,zHighBound;zLowBound,zHighBound],'CData',imread('Items\SafetySign.jpg'),'FaceColor','texturemap');
+
+    % Plot points on the safety sign to follow (use 3).
+
+    P=[xLowBound,   xLowBound,   xHighBound;
+       yConstant, yConstant,  yConstant;
+       zLowBound,  zHighBound,  zLowBound];
+
+    spherePoints_h = plot_sphere(P, 0.01, 'g');
+
+%
+    % Mount the camera onto the Dobot
+cam = CentralCamera('focal', 0.08, 'pixel', 10e-5, ...
+'resolution', [1024 1024], 'centre', [512 512],'name', 'DobotCamera');
+
+cam.plot_camera('Tcam',Tc0, 'label','scale',0.15);
+
+
+    % Begin visual servoing and RMRC retreat for specified steps.
+
+
+end
 
 %% UniversalTravel
 % Function to move both robots at once at varying time steps.
@@ -264,10 +403,10 @@ function UniversalTravel(r1_h ,r1q1, r1q2, r1Steps, r2_h, r2q1, r2q2, r2Steps, m
 
             % Move the designated object that has been picked up. Pass the
             % object's handle to the code.
-
-            verticesAdjustTr = (r1_h.model.fkine(qr1Matrix(r1Index,:))); % Alters last position by this amount.
-            transformedVertices = [self.vertices(:,:,brick),ones(size(self.vertices(:,:,brick),1),1)] * verticesAdjustTr'; 
-            set(self.brick_h(brick,1), 'Vertices', transformedVertices(:,1:3));
+% 
+%             verticesAdjustTr = (r1_h.model.fkine(qr1Matrix(r1Index,:))); % Alters last position by this amount.
+%             transformedVertices = [self.vertices(:,:,brick),ones(size(self.vertices(:,:,brick),1),1)] * verticesAdjustTr'; 
+%             set(self.brick_h(brick,1), 'Vertices', transformedVertices(:,1:3));
             drawnow;
 
         end
@@ -278,10 +417,10 @@ function UniversalTravel(r1_h ,r1q1, r1q2, r1Steps, r2_h, r2q1, r2q2, r2Steps, m
             % Move the designated object that has been picked up. Pass the
             % object's handle to the code.
 
-            verticesAdjustTr = (r2_h.model.fkine(qr2Matrix(r2Index,:))); % Alters last position by this amount.
-            transformedVertices = [self.vertices(:,:,brick),ones(size(self.vertices(:,:,brick),1),1)] * verticesAdjustTr'; 
-            set(self.brick_h(brick,1), 'Vertices', transformedVertices(:,1:3));
-            drawnow;
+%             verticesAdjustTr = (r2_h.model.fkine(qr2Matrix(r2Index,:))); % Alters last position by this amount.
+%             transformedVertices = [self.vertices(:,:,brick),ones(size(self.vertices(:,:,brick),1),1)] * verticesAdjustTr'; 
+%             set(self.brick_h(brick,1), 'Vertices', transformedVertices(:,1:3));
+%             drawnow;
 
         end
     pause(0.01);
@@ -290,61 +429,93 @@ end
 
 %% RMRC
 % Given a handle for a robot, coordinates
-function RMRC(robot, T, q0, steps)
+function lastPose = RMRC(robot, T1, T2, q0, steps, deltaT)
 
-    % Resolved Motion Rate Control
-    % Set steps
-    steps = 100;
-    
-    % Set cartesian start and end points.
-    x1 = [1.6, 1.005, ]';
-    x2 = [1.1, -0.45, ]';
-    deltaT = 0.5;                                        % Discrete time step
-    
+    % Function requires:
+    % - Handle of the robot being used.
+    % - Initial transform 
+    % - Final transform
+    % - Initial guess to help inverse kinematics
+    % - Desired steps
+    % - deltaT to set velocity
+   
+    % Set lowest manipulability threshold.
+    epsilon = 0.1;
+
     % Create Cartesian waypoints for trajectory.
-    x = zeros(2,steps);
-    s = lspb(0,1,steps);                                 % Create interpolation scalar
-    for i = 1:steps
-        x(:,i) = x1*(1-s(i)) + s(i)*x2;                  % Create trajectory in x-y plane
-    end
+    x = zeros(3,steps);
+
+        % Extract Cartesian coordinates from given transforms.
+        x1 = T1(1:3,4);
+        x2 = T2(1:3,4);
     
+        s = lspb(0,1,steps);                                 % Create interpolation scalar
+        for i = 1:steps
+            x(:,i) = x1*(1-s(i)) + s(i)*x2;                  % Create trajectory in x-y-z plane
+        end
+    
+    % Create RPY waypoints for trajectory.
+    theta = zeros(3, steps);
+
+        % Extract RPY from given transforms.
+        theta1 = tr2rpy(T1)';
+        theta2 = tr2rpy(T2)';
+
+        s = lspb(0,1,steps);                % Trapezoidal trajectory scalar
+        for i=1:steps
+            theta(:,i) = theta1*(1-s(i)) + s(i)*theta2;       
+        end
+
+
     % Create pose matrix.
-    qRMatrix = nan(steps,2);
-    T1 = [eye(3) [0 x1']'; zeros(1,3) 1];
-    qRMatrix(1,:) = p2.ikcon(T,q0);                 % Solve for joint angles
+    qRMatrix = nan(steps,6);
+    qRMatrix(1,:) = q0;% From initial guess which is chosen pose.   %robot.model.ikcon(T1,q0);                 % Solve for joint angles
     
+    m = zeros(1,steps-1);
+
     % Use RMRC to move between set Cartesian points.
     for i = 1:steps-1
+        % Get linear velocity
         xdot = (x(:,i+1) - x(:,i))/deltaT;              % Calculate velocity at discrete time step
-        J = robot.jacob0(qRMatrix(i,:));                    % Get the Jacobian at the current state
+        
+        % Get angular velocity
+
+        T = robot.model.fkine(qRMatrix(i,:));
+
+        Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
+        Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
+    
+        Rdot = (1/deltaT)*(Rd - Ra);                                                % Calculate rotation matrix error
+        S = Rdot*Ra';                                                           % Skew symmetric!
+        
+        thetadot = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
+    
+        velocity = [xdot;thetadot];
+
+        J = robot.model.jacob0(qRMatrix(i,:));                    % Get the Jacobian at the current state
 
         m(i) = sqrt(det(J*J'));
         if m(i) < epsilon  % If manipulability is less than given threshold
-            lambda = (1 - m(i)/epsilon)*5E-2;
+            lambda = 0.1;%(1 - m(i)/epsilon)*5E-2;
         else
             lambda = 0;
         end
         invJ = inv(J'*J + lambda *eye(6))*J';                               % DLS Inverse
 
-
-        qdot = inv(J)*xdot;                                                 % Solve velocitities via RMRC
+        qdot = invJ*velocity;                                                 % Solve velocitities via RMRC
         qRMatrix(i+1,:) =  qRMatrix(i,:) + deltaT*qdot';                    % Update next joint state
-    
-        J = robot.jacob0(qRMatrix(i,:));
-  
+      
         for j = 1:6                                                         % Loop through joints 1 to 6
-            if qMatrix(i,j) + deltaT*qdot(i,j) < p560.qlim(j,1)             % If next joint angle is lower than joint limit...
+            if qRMatrix(i,j) + deltaT*qdot(j)' < robot.model.qlim(j,1)             % If next joint angle is lower than joint limit...
                 qdot(i,j) = 0; % Stop the motor
-            elseif qMatrix(i,j) + deltaT*qdot(i,j) > p560.qlim(j,2)         % If next joint angle is greater than joint limit ...
+            elseif qRMatrix(i,j) + deltaT*qdot(j)' > robot.model.qlim(j,2)         % If next joint angle is greater than joint limit ...
                 qdot(i,j) = 0; % Stop the motor
             end
         end
-
-    end
-
-    %T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];          % Create transformation of first point and angle
-    %q0 = zeros(1,6);                                                            % Initial guess for joint angles
-    qMatrix(1,:) = robot.ikcon(T,q0);                                            % Solve joint angles to achieve first waypoint    
+        robot.model.animate(qRMatrix(i,:));
+        pause(0.001);
+    end 
+    lastPose = qRMatrix(end,:);
 end
 
 %% Dobot Travel Method Outside of Class 
