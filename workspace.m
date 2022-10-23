dc81c071e08e7a4b099a9464c11a8a4efd9b1396
Damian Sue <113498238+Sevenstar27@users.noreply.github.com>
2022-10-23 22:24:47 +1100

    Everything except visual servoing and robot control

--- workspace.m
+++ workspace.m
@@ -4,13 +4,25 @@ hold on;
 
 %% Set up controller for input (disable if you don't have the joystick)
 global enableJoy
-enableJoy = 0;
+enableJoy = true;
 id = 1;
 global joy;
 if enableJoy
     joy = vrjoystick(id);
 else
     buttons = zeros(1,16);
+end
+
+%% Plot the collision rectangular prism for dobot. Change its position to test trajectory pathing.
+placeObstacle = true;
+if placeObstacle
+    global vertex;
+    global faces;
+    global faceNormals;
+    global rect_h;    
+    centrePoint = [0.5, 0.3, 1];
+    plotOptions.plotFaces = true;
+    [vertex, faces, faceNormals, rect_h] = RectangularPrism(centrePoint-0.3, centrePoint+0.3, plotOptions);
 end
 
 %% Set up item pickup status globally.
@@ -275,7 +287,7 @@ q1 = zeros(1,6);
 q2 = ones(1,6);
 
 view(142.5, 30);
-
+keyboard; % Wait for approval to begin task.
 %% Navigation for ROZUM picking up soft drink. 
 
 % Extract the transform of the attach point of the soft drink.
@@ -333,7 +345,7 @@ lastPose = RMRC(r, drinkBoxAboveTr*transl(0,0,0.5), drinkBoxAboveTr, lastPose, 4
 
 r.Travel(lastPose, q1, 50);
 
-%% Navigation for ROZUM picking up cutlery (in progress)
+%% Navigation for ROZUM picking up cutlery
 
 % Transform of cutlery container's attach point.
 
@@ -438,6 +450,7 @@ delete(mealBox_Open_h);
 delete(softDrinkCola_h);
 delete(cutlery_h);
 delete(sauce_h);
+%%
 mealBox_ClosedCoords = r.model.base*transl(0,-0.55,0);
 global mealBox_Closed_h;
 %delete(mealBox_Closed_h);
@@ -462,36 +475,51 @@ mealBox_Closed_h = PlaceObject('Items\MealBox_Closed.ply', mealBox_ClosedCoords(
     
     lastPoseQ = RMRC(r, mealBoxHandoverTr, mealBoxHandoverTr*transl(0.3,0,0), [(lastPoseQ(1) +pi) ,lastPoseQ(2:end)], 30, 0.2);
     mealPickup = 0;
+%%
 
 % Dobot
     % Navigate to neutral pose
-    
+    dNeutral = deg2rad([-88, 5, 0, 107, 0]);
+    DTravel(d, d.model.getpos, dNeutral, 50, 1);
+
     % Build pickup transform. 
-    handOverCoords = ; % global (y, x, z)
-    handOverCoords = handOverCoords(1:3,4)';
-    pickup = transl(handOverCoords);
+    handOverTr = r.model.fkine(lastPoseQ); 
+    pickupTr = handOverTr*trotx(pi);
 
+    pickupQ = deg2rad([-88, 20, 52.6, 107, 0]);
+       
+    DTravel(d, dNeutral, pickupQ, 50, 1);
+    mealPickup = 0;
+    r.Travel(lastPoseQ, q1, 40); % Return to neutral pose.
 
-pickup = deg2rad([90, 39.7, 17.2, 30, 0]);
-
+    mealPickup = 1;
 
 % Move to dropoff location. Inspect trajectory for collisions and halt if
 % there is one before moving. 
 
-dropoff1 = [-1, 0, 0.5236, 0.1745, 0.8657, 0];
-dropoff2 = [-1, 0, 0.5236, 0.8727, 0.1745, 0];
+dropoffQ = [(pickupQ(1)+pi), pickupQ(2:end)]; %d.model.ikcon(dropoffTr, pickupQ);
+dropoffTr = d.model.fkine(dropoffQ);
+DTravel(d, pickupQ, dropoffQ, 50, 1);
 
-%DTravel(d, );
-DTravel(d, pickup, dropoff1, 50, 1, r);
-mealPickup = 1;
-DTravel(d, dropoff1, dropoff2, 50, 1, r);
-DTravel(d, dropoff2, dropoff1, 50, 1, r);
-DTravel(d, dropoff1, pickup, 50, 1, r);
+% RMRC to lower and leave the meal box.
+releaseTr = d.model.fkine(dropoffQ)*transl(0,0,-0.02);
+releaseQ = d.model.ikcon(releaseTr, dropoffQ);
+
+DTravel(d, dropoffQ, releaseQ, 20, 1);
+
+%DTravel(d, pickup, dropoff1, 50, 1);
+mealPickup = 0;
+DTravel(d, releaseQ, dropoffQ, 20, 1);
+DTravel(d, dropoffQ, dNeutral, 40, 1);
+
 
 %% VisualServoing test
 itemPickup = 0;
 mealPickup = 0;
 % VisualServoingSafety(r, d);
+
+%% Robot Jogging
+
 
 %% VisualServoingSafety
 % Start the visual servoing safety demonstration. Note that the camera will
@@ -1036,24 +1064,22 @@ end
 
 %% Dobot Travel Method Outside of Class 
 % Only for independent movement of the robot.
-function DTravel(robot, q1, q2, steps, mode, r)
+function DTravel(robot, q1, q2, steps, mode)
     global joy;
     global person_h;
     global r;
-    global d;
     global enableJoy;
+    global mealPickup;
+    global mealBox_Closed_h;
+    global vertex;
+    global faces;
+    global faceNormals;
+    global rect_h;
 
 
     % mode:
     %   1 - Trapezoidal
     %   2 - Quintic Polynomial
-
-    % TO DO LIST:
-    % - Add choice between trapezoidal, quintic polynomial, RMRC.
-    % - Manipulability measure for singularities and DLS
-    % - Move simultaneously with the other robot and all other
-    % objects with some universal motion function. 
-    % - Attach points for items and grippers.
 
     if mode == 1
         s = lspb(0,1,steps);    % Create matrix describing trapezoidal trajectory 
@@ -1129,10 +1155,156 @@ function DTravel(robot, q1, q2, steps, mode, r)
                         end
                     end
                 end
+            if mealPickup == 1 % Pickup the meal with specified robot.
+                % Delete instance, replot at origin, and transform to end
+                % effector.
+                delete(mealBox_Closed_h);
+                mealBox_Closed_h = PlaceObject('Items\MealBox_Closed.ply'); 
+    
+                vertices = get(mealBox_Closed_h, 'vertices');
+                verticesAdjustTr = robot.model.fkine(qMatrix(i,:))*transl(0,0,-0.23)*trotz(pi/2);
+                transformedVertices = [vertices,ones(size(vertices,1),1)] * verticesAdjustTr'; 
+                set(mealBox_Closed_h, 'vertices', transformedVertices(:,1:3));
+                drawnow();            
+            end
+            % Check for collision with the human cube.
+
+            dobotCollide = collisionCheck(robot, qMatrix, faces, vertex, faceNormals, true);
+            while dobotCollide
+                % Continually check collisionCheck for a collision.
+                dobotCollide = collisionCheck(robot, qMatrix, faces, vertex, faceNormals, true);
+                pause(0.1); % Allow for ui use.
+                if enableJoy % turn off the obstacle with the controller.
+                    [axes, buttons, povs] = read(joy);
+                    if buttons(3) == 1 % IF X IS PRESSED, DELETE OBSTACLE
+                        delete(rect_h);
+                        faces = [];
+                        vertex = [];
+                        faceNormals = [];
+                    end
+                end
+            end
+
 
             % If permitted to proceed, resume animating the robot.
-
+            
            robot.model.animate(qMatrix(i,:));
+           pause(0.01);
            drawnow;
        end
+end
+
+%% collisionCheck
+% Given a robot model (robot) and trajectory (qMatrix), as well as triangle obstacles in the
+% environment (faces, vertex, faceNormals)
+function result = collisionCheck(robot, qMatrix, faces, vertex, faceNormals, returnOnceFound)
+    if nargin < 6
+        returnOnceFound = true;
+    end
+    result = false;
+
+    for qIndex = 1:size(qMatrix, 1); % For each row/step in the qMatrix,
+        % Get the transform of every joint (starting and ending transform
+        % of every link)
+        tr = GetLinkPoses(qMatrix(qIndex, :), robot);
+
+        % Go through each link and also each triangle face
+        for i=1:size(tr,3)-1
+            for faceIndex = 1:size(faces, 1)
+                vertOnPlane = vertex(faces(faceIndex,1)',:);
+                [intersectP, check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+                if (check == 1) && (IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:)))
+                    plot3(intersectP(1), intersectP(2), intersectP(3), 'g*');
+                    %display('Intersection');
+                    result = true;
+                    if returnOnceFound
+                        return
+                    end
+                end
+            end
+        end
+    end
+end
+
+%% GetLinkPoses
+% q - robot joint angles
+% robot - SerialLink robot model
+% transforms - list of transforms
+function [transforms] = GetLinkPoses(q, robot)
+    links = robot.model.links;
+    transforms = zeros(4,4,length(links)+1);
+    transforms(:,:,1) = robot.model.base;
+    for i = 1:length(links)
+        L = links(1,i);
+
+        current_transform = transforms(:,:,i);
+
+        % Apply DH parameters to make transforms along robot links.
+        current_transform = current_transform * trotz(q(1,i) + L.offset) * transl(0,0,L.d) * transl(L.a,0,0) * trotx(L.alpha);
+        transforms(:,:,i+1) = current_transform;
+    end
+end
+
+%% IsIntersectionPointInsideTriangle
+% Given a point which is known to be on the same plane as the triangle
+% determine if the point is 
+% inside (result == 1) or 
+% outside a triangle (result ==0 )
+function result = IsIntersectionPointInsideTriangle(intersectP,triangleVerts)
+    u = triangleVerts(2,:) - triangleVerts(1,:);
+    v = triangleVerts(3,:) - triangleVerts(1,:);
+    uu = dot(u,u);
+    uv = dot(u,v);
+    vv = dot(v,v);
+    w = intersectP - triangleVerts(1,:);
+    wu = dot(w,u);
+    wv = dot(w,v);
+    D = uv * uv - uu * vv;
+    % Get and test parametric coords (s and t)
+    s = (uv * wv - vv * wu) / D;
+    if (s < 0.0 || s > 1.0)        % intersectP is outside Triangle
+        result = 0;
+        return;
+    end
+    t = (uv * wu - uu * wv) / D;
+    if (t < 0.0 || (s + t) > 1.0)  % intersectP is outside Triangle
+        result = 0;
+        return;
+    end
+    result = 1;                      % intersectP is in Triangle
+end
+
+%% LinePlaneIntersection
+% Given a plane (normal and point) and two points that make up another line, get the intersection
+% Check == 0 if there is no intersection
+% Check == 1 if there is a line plane intersection between the two points
+% Check == 2 if the segment lies in the plane (always intersecting)
+% Check == 3 if there is intersection point which lies outside line segment
+function [intersectionPoint,check] = LinePlaneIntersection(planeNormal,pointOnPlane,point1OnLine,point2OnLine)
+
+intersectionPoint = [0 0 0];
+u = point2OnLine - point1OnLine;
+w = point1OnLine - pointOnPlane;
+D = dot(planeNormal,u);
+N = -dot(planeNormal,w);
+check = 0; %#ok<NASGU>
+if abs(D) < 10^-7        % The segment is parallel to plane
+    if N == 0           % The segment lies in plane
+        check = 2;
+        return
+    else
+        check = 0;       %no intersection
+        return
+    end
+end
+
+%compute the intersection parameter
+sI = N / D;
+intersectionPoint = point1OnLine + sI.*u;
+
+if (sI < 0 || sI > 1)
+    check= 3;          %The intersection point  lies outside the segment, so there is no intersection
+else
+    check=1;
+end
 end
